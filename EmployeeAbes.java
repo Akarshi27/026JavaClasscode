78eb0d68fe030b28e08201b6f2dca04a6b92b0f9
Akarshi27 <akarshisrivastava2711@gmail.com>
2025-08-25 10:15:40 +0530

    Add files via upload

---
+++ EmployeeAbes.java
@@ -0,0 +1,20 @@
+package ABES;
+import kiet.*;
+public class EmployeeAbes extends EmployeeKiet {
+    public static void main(String[] args) {
+       // EmployeeAbes ae= new EmployeeAbes();
+        EmployeeKiet ek=new EmployeeKiet();
+        ek.Emp_name="Shikha";
+        /*ek.Emp_id=102;
+        ek.Salary=200000;
+        ek.Organisation="KIET groups";*/
+
+
+      /*  ae.Emp_name="Shikha";
+        ae.Emp_id=102;
+        ae.Salary=200000;
+        ae.Organisation="KIET groups";
+*/
+    }
+    
+}
